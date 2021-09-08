2c6291d69dbdac8ff4525023bc97dcdaed79c64e
tmurray77 <89397724+tmurray77@users.noreply.github.com>
2021-09-08 11:45:41 -0500

    ClassSept08
    Dropdown menu. JS if/else to act on it: .change(), .slideUp(), .css('key', 'value'), .addClass('name'), .slideDown(), .removeClass('name').slideDown()

--- ProfileExample/js/app.js
+++ ProfileExample/js/app.js
@@ -4,3 +4,16 @@ $('#btnTest').click(function() {
     let decRate = $('#txtPayRate').val() ;
     console.log(decHours * decRate);
 });
+//$('#txtPayRate').change(function(){
+//    alert($('#txtPayRate').val());
+//});
+$('#cboEmployeeType').change(function(){
+    if($('#cboEmployeeType').val() == 'FULL'){
+       // $('#divHours').slideUp();
+       // $('#divHours').css('display', 'none');
+       $('#divHours').addClass('d-none');
+    }else{
+        //$('#divHours').slideDown();
+        $('#divHours').removeClass('d-none').slideDown();
+    }
+});
